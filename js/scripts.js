2ba0d7378e87fded550bf0c01c2713e7a2626499
Al-Web-Dev <32247362+Al-Web-Dev@users.noreply.github.com>
2018-06-16 22:04:01 +0100

    first upload header build

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+(function($) {
+    "use strict";
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 60
+    });
+
+    $('#topNav').affix({
+        offset: {
+            top: 200
+        }
+    });
+    
+    new WOW().init();
+    
+    $('a.page-scroll').bind('click', function(event) {
+        var $ele = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($ele.attr('href')).offset().top - 60)
+        }, 1450, 'easeInOutExpo');
+        event.preventDefault();
+    });
+    
+    $('.navbar-collapse ul li a').click(function() {
+        /* always close responsive nav after click */
+        $('.navbar-toggle:visible').click();
+    });
+
+    $('#galleryModal').on('show.bs.modal', function (e) {
+       $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
+    });
+
+})(jQuery);
+
+// script for background hover effect
+
+$(document).ready(function() {
+var movementStrength = 5;
+var height = movementStrength / $(window).height();
+var width = movementStrength / $(window).width();
+$("header").mousemove(function(e){
+          var pageX = e.pageX - ($(window).width() / 2);
+          var pageY = e.pageY - ($(window).height() / 2);
+          var newvalueX = width * pageX * -1 - 5;
+          var newvalueY = height * pageY * -1 - 5;
+          $('header').css("background-position", newvalueX+"px     "+newvalueY+"px");
+});
+});
